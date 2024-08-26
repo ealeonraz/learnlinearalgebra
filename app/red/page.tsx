9d279bffbc972fd42f44370980af3a59965fb76f
ealeonraz <ezequielalcarazleon@gmail.com>
2024-08-25 21:02:46 -0700

    Google analytics

--- app/red/page.tsx
+++ app/red/page.tsx
@@ -1,7 +1,30 @@
+import Script from 'next/script';
+
 export default function Home() {
   return (
-    <div>
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-WXRPDT3LXF"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WXRPDT3LXF');
+          `,
+        }}
+      />
+      
+      {/* Content */}
+      <div>
         STOP STALKING ME!
-    </div>
-  )
+      </div>
+    </>
+  );
 }
