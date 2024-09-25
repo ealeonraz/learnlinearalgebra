1098422892de17568486b238a1f676d92b99d18a
ealeonraz <ezequielalcarazleon@gmail.com>
2024-09-24 23:49:33 -0700

    Added Key Props

--- app/fundamental-concepts-linear-algebra/matrix-equation-axb/page.tsx
+++ app/fundamental-concepts-linear-algebra/matrix-equation-axb/page.tsx
@@ -9,10 +9,11 @@ export default async function View() {
     const theorem: TheoremInfo[] = await getProofs(section);
     
     return (
-        <div className={"proof_container"}>
-            <div className={"proof_card"}>
-                {theorem.map((proof) => (
+        <div className="proof_container">
+            <div className="proof_card">
+                {theorem.map((proof, index) => (
                     <DisplayProof
+                    key = {index}
                     theorem = {proof.theorem}
                     proof_content = {proof.proof_content}
                     theorem_name={proof.theorem_name}
--- app/fundamental-concepts-linear-algebra/row-reductions-and-echelon-forms/page.tsx
+++ app/fundamental-concepts-linear-algebra/row-reductions-and-echelon-forms/page.tsx
@@ -11,8 +11,9 @@ export default async function View() {
     return (
         <div className="proof_container">
             <div className="proof_card">
-                {theorem.map((proof) => (
+                {theorem.map((proof, index) => (
                     <DisplayProof
+                    key = {index}
                     theorem = {proof.theorem}
                     proof_content = {proof.proof_content}
                     theorem_name={proof.theorem_name}
