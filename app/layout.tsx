cfebcdaeb78cce5019e9a2f6253d672bfa4179f6
ealeonraz <ezequielalcarazleon@gmail.com>
2024-12-14 17:57:34 -0800

    Fixed deleted import statement

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import ClientLayout from "./ClientLayout"; // Import the client layout
 
 const inter = Inter({ subsets: ["latin"] });
 
